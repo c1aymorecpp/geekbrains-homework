9b835c1d893edca6f51689375a4b5ccac0a49dec
c1aymore <c1aymorefn@gmail.com>
2021-03-17 14:16:11 +0300

    add lesson2 homework

---
+++ Занятие 2/Задание 1/Задание 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пожалуйста мнимальную температуру за сутки"); 
+            int minTemp = Convert.ToInt32(Console.ReadLine()); // пользователь вводит минимальную температуру
+            Console.WriteLine("Введите пожалуйста максимальную температуру за сутки");
+            int maxTemp = Convert.ToInt32(Console.ReadLine()); // пользователь вводит максимальную температуру
+            int avgTemp = (maxTemp + minTemp) / 2; // получаем среднюю температуру
+            Console.WriteLine($"Среднесуточная температура: = {avgTemp}");
+        }
+    }
+}
---
+++ Занятие 2/Задание 2/Задание 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите пожалуйста порядковый номер текущего месяца.");
+            int numberOfMonth = Convert.ToInt32(Console.ReadLine());
+            string monthTitle = null;
+            switch(numberOfMonth)
+            {
+                case 1:
+                    monthTitle = "Январь";
+                    break;
+                case 2:
+                    monthTitle = "Февраль";
+                    break;
+                case 3:
+                    monthTitle = "Март";
+                    break;
+                case 4:
+                    monthTitle = "Апрель";
+                    break;
+                case 5:
+                    monthTitle = "Май";
+                    break;
+                case 6:
+                    monthTitle = "Июнь";
+                    break;
+                case 7:
+                    monthTitle = "Июль";
+                    break;
+                case 8:
+                    monthTitle = "Август";
+                    break;
+                case 9:
+                    monthTitle = "Сентябрь";
+                    break;
+                case 10:
+                    monthTitle = "Октябрь";
+                    break;
+                case 11:
+                    monthTitle = "Ноябрь";
+                    break;
+                case 12:
+                    monthTitle = "Декабрь";
+                    break;
+            }
+            Console.WriteLine(monthTitle);
+        }
+    }
+}
