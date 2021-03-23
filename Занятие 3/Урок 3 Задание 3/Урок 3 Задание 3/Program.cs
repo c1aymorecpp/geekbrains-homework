1eda80699957d005bf6193b201d6723fd4ff895a
c1aymore <c1aymorefn@gmail.com>
2021-03-23 17:16:04 +0300

    загрузил домашнюю работу по 3 занятию

---
+++ Занятие 3/Урок 3 Задание 3/Урок 3 Задание 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace Урок_3_Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = null;
+            string result = null;
+                Console.WriteLine("Введите строку");
+                word = Console.ReadLine();
+                    for (int i = word.Length - 1; i >= 0; i--)
+                    { 
+                        result += word[i];
+                    }
+                    Console.WriteLine(result);
+        }
+    }
+}
