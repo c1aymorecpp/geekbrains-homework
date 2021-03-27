862b4656bbc24b65f776608becaa04f2e38fb9e5
c1aymore <c1aymorefn@gmail.com>
2021-03-27 20:01:52 +0300

    homework lesson4 uploaded

---
+++ Занятие 4/Урок 4 Задание 1/Урок 4 Задание 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Урок_4_Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Введите имя");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Введите фамилию");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Введите отчество");
+                string patronymic = Console.ReadLine();
+                string fullName = GetFullName(firstName, lastName, patronymic);
+                Console.WriteLine(fullName);
+            }
+        }
+
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return (lastName + " " + firstName + " " + patronymic);
+        }
+    }
+}
+
+
---
+++ Занятие 4/Число Фибоначчи/Число Фибоначчи/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Число_Фибоначчи
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Фибоначчи под номером #{0} это {1}", n, Fibonachi(n)); // вывод результата
+        }
+        
+        static int Fibonachi(int n, int x1 = 0, int x2 = 1) // вычисление числа фибоначчи
+        {
+            {
+                if (n <= 1) return x1;
+                int x;
+                for (int i = 2; i < n; i++)
+                {
+                    x = x1;
+                    x1 = x2;
+                    x2 = x2 + x;
+                }
+                return x2;
+            }
+        }
+    }
+}
