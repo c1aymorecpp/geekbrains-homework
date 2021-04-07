5c5a79f7b012ae989865ae4f3f190336086e5344
c1aymore <c1aymorefn@gmail.com>
2021-04-07 19:52:49 +0300

    loaded 5 lesson homework

---
+++ Занятие 5/Урок 5 Задание 2/Урок 5 Задание 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Урок_5_Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            currentTime();
+        }
+
+        static void currentTime() // метод по записи текущего времени в файл
+        {
+            string filename = "startup.txt";
+            string time = DateTime.Now.ToLongTimeString();
+            File.WriteAllText(filename, "Эта программа дописывает текущее время на следующей строке.");
+            File.AppendAllText(filename, Environment.NewLine); // вставляем перенос строк
+            File.AppendAllText(filename, time); // записываем в файл текущее время
+        }
+    }
+}
